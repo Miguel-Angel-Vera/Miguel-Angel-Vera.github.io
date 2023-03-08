72e9e0bf69b9163b74b3b4c9c12f637314890852
Miguel-Angel-Vera <126531201+Miguel-Angel-Vera@users.noreply.github.com>
2023-03-07 22:13:47 -0300

    initial commit

---
+++ js/mostrar.js
@@ -0,0 +1,13 @@
+function mostrar() {
+
+var cert = document.getElementById('certificados');
+
+cert.style.display = "block";
+    
+}
+
+function ocultar() {
+    var cert = document.getElementById('certificados');
+    
+    cert.style.display = "none";
+}
